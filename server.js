7c69ba453e736cbf2eae6ec59c7158f8a15ee968
Raghavender <raghavenderreddylonka.1@gmail.com>
2022-09-23 12:59:10 +0530

    initial

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express")
+
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.json([
+    {
+      id: "001",
+      name: "Raghavender",
+      role: "DevOps Engineer",
+    },
+  ])
+})
+
+app.listen(8080, () => {
+  console.log("listning from port 9000")
+})
